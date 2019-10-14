b77dce24fca63f11081f8718da5edc3f53515907
meisam <softrunapps@gmail.com>
2019-10-14 16:35:45 +0330

    add user model

---
+++ app/src/main/java/com/softrunapps/paginatedrecyclerviewapplication/User.java
@@ -0,0 +1,26 @@
+package com.softrunapps.paginatedrecyclerviewapplication;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
